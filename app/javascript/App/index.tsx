77530b39e19c7fefc534d0ba26282307c34fc490
Aly Badawy <AlyBadawy@users.noreply.github.com>
2023-03-04 10:30:40 -0500

    Setup RTK

--- app/javascript/App/index.tsx
+++ app/javascript/App/index.tsx
@@ -1,13 +1,17 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { App } from './App';
+import { store } from '../store/store';
+import { Provider } from 'react-redux';
 
 document.addEventListener('DOMContentLoaded', () => {
   const rootEl = document.getElementById('root');
   const root = ReactDOM.createRoot(rootEl!);
   root.render(
     <React.StrictMode>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </React.StrictMode>
   );
 });
